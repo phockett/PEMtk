e7f2f1bd9ff787375261fd88af88a409cab505a1
phockett <phockett@users.noreply.github.com>
2021-10-08 10:49:44 -0400

    Testing Alabaster RTD theme.

--- docs/doc-source/conf.py
+++ docs/doc-source/conf.py
@@ -89,9 +89,11 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
+html_theme = 'alabaster'
 # html_theme = 'sphinx_rtd_theme'
-html_theme = "sphinx_book_theme"
+# html_theme = "sphinx_book_theme"  # Builds OK, but lacking heading formatting? Need to set options or add CSS?
+                                    # E.g. https://github.com/jcmgray/xyzpy/blob/develop/docs/_static/my-styles.css
+                                    # TODO: check theme docs, https://sphinx-book-theme.readthedocs.io/en/latest/
 
 html_logo = 'figs/ePSproc_logo.png'
 
